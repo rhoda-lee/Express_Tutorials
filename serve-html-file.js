87173a42cf9d1fc0735eefabc4bd47c3e8c75797
rhoda-lee <rhodalee.dev@gmail.com>
2025-02-05 22:43:35 +0000

    serving html files

---
+++ serve-html-file.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+})
+
+app.listen(port, () => {
+    `listening on port ${port}`;
+});
