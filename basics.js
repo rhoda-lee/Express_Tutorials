38a78e4a7c3ad94228c7ed9d954ac27d8ccc0681
rhoda-lee <rhodalee.dev@gmail.com>
2025-02-05 22:43:21 +0000

    updated file

--- basics.js
+++ basics.js
@@ -1,11 +1,16 @@
+// Get express module
 const express = require('express');
+// Instantiate express app
 const app = express();
+// Define port number
 const port = 3000;
 
+// Define home endpoint
 app.get('/', (req, res) => {
     res.send("Started Express Application!")
 });
 
+// Listen for incoming connections on specified port(3000)
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);
 });
